faaa73cecd41edbc0339832ed2897b7dda27a4e3
Wilhelm Murdoch <wilhelm@devilmayco.de>
2022-05-10 13:48:40 +1000

    more progress

--- template/functions.go
+++ template/functions.go
@@ -24,7 +24,7 @@ func EstimateReadingTime(text string) string {
 	pattern, _ := regexp.Compile(`[^a-zA-Z0-9\s]+`)
 	words := strings.Fields(pattern.ReplaceAllString(text, ""))
 
-	return fmt.Sprintf("%.0f mins", math.Ceil(float64(len(words))/float64(238)))
+	return fmt.Sprintf("%.0f", math.Ceil(float64(len(words))/float64(238)))
 }
 
 func Unescape(text string) template.HTML {
